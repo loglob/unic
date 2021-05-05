79d981c013fda49f7f73c6003be453c8adfe35e5
loglob <23386237+loglob@users.noreply.github.com>
2021-05-06 01:09:52 +0200

    Fixed main() signature

--- test/test.c
+++ test/test.c
@@ -4,7 +4,7 @@ char mul_apin[] = "\xF0\x92\x80\xAF" "\xF0\x92\x80\xB3";
 char ueber[] = "Deutschland, Deutschland " "\xC3\xBC" "ber alles; " "\xC3\x0c" "ber alles in der Welt";
 
 
-int main(int argc, char **argv)
+int main(void)
 {
 	test_utf8();
 	printf("Completed all tests!\n");
