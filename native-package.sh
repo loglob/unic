6a9f9cf9992501a2f8e80472f844ab41ba5b3695
loglob <23386237+loglob@users.noreply.github.com>
2026-08-22 00:14:03 +0200

    Package cross-platform and source packages

--- native-package.sh
+++ native-package.sh
@@ -9,15 +9,12 @@ fi
 set -e
 
 export OPT_CFLAGS="-O3 -march=x86-64-v2 -mtune=generic -flto"
-make codegen
 make -j4 build
 make doc
 
-tar czf "unic-$1-linux-amd64".tar.gz \
+tar czf "unic-$1-$(uname -s)-$(uname -m)".tar.gz \
   README.md \
   LICENSE \
   include \
-  --transform 's|^out/|lib/|' out/libunic.so out/libunic.a \
   --transform 's|^doc/||' doc/man \
-  src \
-  --transform 's|^src-gen|src|' src-gen
+  --transform 's|^out/|lib/|' out/libunic.so out/libunic.a \
---
+++ source-package.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+set -e
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 [version]" 1>&2
+    exit 1
+fi
+
+make codegen
+
+# build package in a temp dir 
+DIR="$(mktemp -d)"
+
+cp -r README.md LICENSE src include "$DIR/"
+cp src-gen/* "$DIR/src/"
+
+# copy over compile flags, sans include flags
+FLAGS="$(grep -v '^-I' compile_flags.txt | tr '\n' ' ')"
+
+# generate a minimal distributable makefile
+cat > "$DIR/makefile" << EOF
+OBJECTS = \$(patsubst src/%.c, build/%.o, \$(wildcard src/*.c))
+
+lib/libunic.a: \$(OBJECTS)
+	mkdir -p lib
+	ar rcs \$@ $^
+
+lib/libunic.so: \$(OBJECTS)
+	mkdir -p lib
+	cc -flto -shared \$^ -o \$@ 
+
+build/%.o: src/%.c src/*.h include/*
+	mkdir -p build
+	cc $FLAGS -O3 -march=native -mtune=native -flto -Iinclude -c $< -o \$@
+EOF
+
+tar -C "$DIR" -czf "unic-$1-source".tar.gz --transform 's|^.||' .
+rm -r "$DIR"
